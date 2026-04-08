2ce9223aa1c2135f412fbcb6ce54bbb1fce75ddd
Ban451 <amadeobrianranastyaputra@gmail.com>
2026-04-08 11:20:13 +0700

    main.dart

--- main.dart
+++ main.dart
@@ -15,33 +15,8 @@ class MyApp extends StatelessWidget {
       title: 'StayVue',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: const Color(0xFF1E3A8A), // Biru tua yang lebih elegan
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: 'Poppins',
-        textTheme: GoogleFonts.poppinsTextTheme().copyWith(
-          bodyLarge: GoogleFonts.poppins(color: Colors.black87),
-          bodyMedium: GoogleFonts.poppins(color: Colors.black54),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: Colors.grey[50],
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color(0xFF2563EB), // Biru yang lebih cerah untuk tombol
-            foregroundColor: Colors.white,
-            minimumSize: const Size(double.infinity, 50),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(25),
-            ),
-            elevation: 0,
-          ),
-        ),
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        primarySwatch: Colors.blue,
       ),
       home: const SplashScreen(),
     );
